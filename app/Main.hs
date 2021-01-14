9ca43e2a7335dac3bdf9ba18d902db5d7dddcc4d
Agnieszka Jurkiewicz <egi.megi@gmail.com>
2021-01-14 22:28:35 +0100

    Read line of txt file to array

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,7 @@
 import Prelude
 import System.IO
+import Data.Char(digitToInt)
+
 -- wczytaj łamigłówkę z pliku o podanej nazwie
 readPuzzle :: String -> IO[String]
 readPuzzle filename = do
@@ -8,21 +10,26 @@ readPuzzle filename = do
   return puzzle 
 
 zero x y = 0 -- macierz samych 0
-set array x y c = (\x1 y1 -> if ((x==x1) && (y==y1)) then c else array x y )
+set array x y c =  (\x1 y1 -> if ((x==x1) && (y==y1)) then c else array x1 y1 )
 
 -- wczytaj długosć i szerokosć tabeli
 puzzleLength p = length p
 puzzleWidth pw = length $ head $ pw
 
+readLineToArray (c:xs) ar x y | c/='.' = readLineToArray xs (set ar x y (digitToInt c)) (x+1) y | otherwise = readLineToArray xs ar (x+1) y  
+readLineToArray [] ar x y = ar
 
 
-input = set zero 1 4 7 -- macierz wejściowa
+input = set (set zero 1 4 7) 2 3 8 -- macierz wejściowa
 
 main = do 
   puzzle <- readPuzzle "puzzle.txt" 
   let l = puzzleLength puzzle
   let w = puzzleWidth puzzle
+  putStrLn(show $ digitToInt $ head $ tail $ tail $ head puzzle)
+  putStrLn(show $ ((readLineToArray (head puzzle) zero 0 0) 2 0 ))
   putStrLn(show $ w)
   putStrLn(show $ l) -- wyświetl liczbę wierszy łamigłówki
   putStrLn(show $ (input 1 1) ) 
-  putStrLn(show $ (input 1 4)) 
+  putStrLn(show $ (input 1 4))
+  putStrLn(show $ (input (-2) (-3))) 
