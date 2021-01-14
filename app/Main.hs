f73abbe7edf75c371ea54895e684ce2944f6871e
Agnieszka Jurkiewicz <egi.megi@gmail.com>
2021-01-14 22:41:00 +0100

    Put all lines from txt file to array

--- app/Main.hs
+++ app/Main.hs
@@ -10,15 +10,21 @@ readPuzzle filename = do
   return puzzle 
 
 zero x y = 0 -- macierz samych 0
+
+-- wstawianie liczb do macierzy
 set array x y c =  (\x1 y1 -> if ((x==x1) && (y==y1)) then c else array x1 y1 )
 
 -- wczytaj długosć i szerokosć tabeli
 puzzleLength p = length p
 puzzleWidth pw = length $ head $ pw
 
+-- wczytywanie pierwszego wiersza z pliku wejściowego *.txt
 readLineToArray (c:xs) ar x y | c/='.' = readLineToArray xs (set ar x y (digitToInt c)) (x+1) y | otherwise = readLineToArray xs ar (x+1) y  
 readLineToArray [] ar x y = ar
 
+-- wczytywanie wszystkich wierszy z pliku wejściowego *.txt
+readAllLinesToArray (z:zs) ar x y = readAllLinesToArray zs (readLineToArray z ar x y) x (y+1)
+readAllLinesToArray [] ar x y = ar
 
 input = set (set zero 1 4 7) 2 3 8 -- macierz wejściowa
 
@@ -26,7 +32,7 @@ main = do
   puzzle <- readPuzzle "puzzle.txt" 
   let l = puzzleLength puzzle
   let w = puzzleWidth puzzle
-  putStrLn(show $ digitToInt $ head $ tail $ tail $ head puzzle)
+  putStrLn(show $ ((readAllLinesToArray puzzle zero 0 0) 6 9 ))
   putStrLn(show $ ((readLineToArray (head puzzle) zero 0 0) 2 0 ))
   putStrLn(show $ w)
   putStrLn(show $ l) -- wyświetl liczbę wierszy łamigłówki
